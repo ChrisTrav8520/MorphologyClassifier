74315d751424367489f6d007059da36d582a8be0
ChrisTrav8520 <115077234+ChrisTrav8520@users.noreply.github.com>
2024-06-06 20:31:05 +0530

    Update app.py
    Centered the logo

--- src/app.py
+++ src/app.py
@@ -75,7 +75,7 @@ st.markdown(
 )
 st.write('')
 st.write('<span style="font-size:18px;">Thank you for using our model. This ML model was developed by Bishnu R under the supervision of Professor Arnab Dutta (in collaboration with Professor Nandini Bhandaru) of BITS Pilani, Hyderabad Campus. This webapp can be used to predict morphologies of PS/PMMA blends. The classes being predicted are - Column, Hole and Island.</span>', unsafe_allow_html=True)
-st.write('<span style="font-size:18px;">The input requirements are as follows:</span>', unsafe_allow_html=True)
+st.write('<span style="font-size:18px;">Kindly ensure that all feature values are positive. The input requirements for the features are as follows:</span>', unsafe_allow_html=True)
 st.write('<span style="font-size:18px;">1. The concentration must be input as %</span>', unsafe_allow_html=True)
 st.write('<span style="font-size:18px;">2. The weight fraction of PS must lie between 0 and 1 as it is a ratio</span>', unsafe_allow_html=True)
 st.write('<span style="font-size:18px;">3. The PMMA molecular weight must be input in the units of Da</span>', unsafe_allow_html=True)
